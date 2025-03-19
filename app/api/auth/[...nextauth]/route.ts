0b641f1dcfd789751ba67a610303e5cc06b12d14
Starlee Jiles <starlee.jiles@icloud.com>
2025-03-19 01:49:39 +0000

    add github auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,16 @@
-import { handlers } from "@/auth"; 
-export const { GET, POST } = handlers;
-export const runtime = "edge";
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+const authOptions = {
+  secret: process.env.AUTH_SECRET, // or process.env.NEXTAUTH_SECRET (ensure consistency)
+  providers: [
+    GitHubProvider({
+      clientId: process.env.AUTH_GITHUB_ID!,
+      clientSecret: process.env.AUTH_GITHUB_SECRET!,
+    }),
+  ],
+  debug: true,
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
--- auth.ts
+++ auth.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 
 export const { handlers, auth } = NextAuth({
-  secret: process.env.NEXTAUTH_SECRET, // Required!
+  secret: process.env.NEXTAUTH_SECRET, // or NEXTAUTH_SECRET
   providers: [
     GitHubProvider({
       clientId: process.env.AUTH_GITHUB_ID!,
